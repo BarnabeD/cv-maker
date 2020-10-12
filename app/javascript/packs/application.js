5fe6bb1c7a70046e5206d8e15ed1e97098c0cf26
Barnabe Dubus <barnabe.dubus@gmail.com>
2020-10-12 23:42:20 +0200

    Wip installing Tailwind

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,4 +14,4 @@ require("@rails/activestorage").start()
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-require("css/application.scss")
+require("../css/application.scss")
