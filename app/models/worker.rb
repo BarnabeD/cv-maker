d8ba6e531ac91a25443864d3ea59b9c3e9733201
Barnabe Dubus <barnabe.dubus@gmail.com>
2020-10-12 19:59:22 +0200

    worker controller + worker and site routes

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -2,4 +2,5 @@ class Worker < ApplicationRecord
   has_many :positions
   has_many :graduates
   has_many :trainings
+  has_many :sites, through: positions
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :collaborateurs, controller: 'workers', as: 'workers' do
+  # resources :workers, as: 'salarie' do
+    resources :chantier, controller: 'sites', as: 'sites'
+  end
 end
