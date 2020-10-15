d55c23615ff2d02e3cf710b73b6039bbfa96073a
Barnabe Dubus <barnabe.dubus@gmail.com>
2020-10-15 17:36:09 +0200

    View generated for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  unauthenticated do
+    root to: 'pages#home'
+  end
+
+  authenticated do
+    root to: 'workers#index', as: :authenticated_root
+  end
+
   resources :collaborateurs, controller: 'workers', as: 'workers' do
     resources :chantier, controller: 'sites', as: 'sites'
   end
