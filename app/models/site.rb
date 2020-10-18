5790e6207beea159a23efe02fcc007a8da928c64
Barnabe Dubus <barnabe.dubus@gmail.com>
2020-10-18 20:08:49 +0200

    nested form for compagny/client/position inside site#new

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,4 +1,7 @@
 class Site < ApplicationRecord
   belongs_to :client
-  has_many :positions
+  has_many :positions, inverse_of: :site
+  has_many :workers, through: :positions
+  has_many :companies, through: :positions
+  accepts_nested_attributes_for :positions, :client, :companies
 end
