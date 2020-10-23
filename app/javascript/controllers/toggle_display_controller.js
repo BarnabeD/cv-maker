bb9f113a350cae0c7173830478e7b74abe2c5817
Barnabe Dubus <barnabe.dubus@gmail.com>
2020-10-24 01:35:37 +0200

    rewrite stimulus controller to add graduate form

--- app/javascript/controllers/toggle_display_controller.js
+++ app/javascript/controllers/toggle_display_controller.js
@@ -1,17 +1,29 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ 'form', 'newButton', 'cancelButton' ]
+  static targets = [ 'graduateForm', 'positionForm', 'newPositionButton', 'newGraduateButton','cancelPositionButton', 'cancelGraduateButton' ]
 
-  toggleShow() {
-    if (this.formTarget.classList.contains('hidden')) {
-      this.formTarget.classList.replace( 'hidden', 'display' )
-      this.cancelButtonTarget.classList.replace( 'hidden', 'display' )
-      this.newButtonTarget.classList.toggle('hidden')
-    } else if (this.formTarget.classList.contains('display')) {
-      this.formTarget.classList.replace( 'display', 'hidden' )
-      this.cancelButtonTarget.classList.toggle('hidden')
-      this.newButtonTarget.classList.toggle('hidden')
+  togglePositionShow() {
+    if (this.positionFormTarget.classList.contains('hidden')) {
+      this.positionFormTarget.classList.replace( 'hidden', 'display' )
+      this.cancelPositionButtonTarget.classList.replace( 'hidden', 'display' )
+      this.newPositionButtonTarget.classList.toggle('hidden')
+    } else if (this.positionFormTarget.classList.contains('display')) {
+      this.positionFormTarget.classList.replace( 'display', 'hidden' )
+      this.cancelPositionButtonTarget.classList.toggle('hidden')
+      this.newPositionButtonTarget.classList.toggle('hidden')
+    }
+  }
+
+  toggleGraduateShow() {
+    if (this.graduateFormTarget.classList.contains('hidden')) {
+      this.graduateFormTarget.classList.replace( 'hidden', 'display' )
+      this.cancelGraduateButtonTarget.classList.replace( 'hidden', 'display' )
+      this.newGraduateButtonTarget.classList.toggle('hidden')
+    } else if (this.graduateFormTarget.classList.contains('display')) {
+      this.graduateFormTarget.classList.replace( 'display', 'hidden' )
+      this.cancelGraduateButtonTarget.classList.toggle('hidden')
+      this.newGraduateButtonTarget.classList.toggle('hidden')
     }
   }
 }
