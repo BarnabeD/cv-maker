a44a757c4ff8622292361bd001618f33698eab74
Barnabe Dubus <barnabe.dubus@gmail.com>
2020-10-23 15:45:42 +0200

    controller rewriting for nested form => entry point position#new

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,64 +1,38 @@
 class SitesController < ApplicationController
-  before_action :set_site, only: [:show, :edit, :update, :destroy]
+  # before_action :set_site, only: [:show, :edit, :update, :destroy]
 
-  def create
-    @site = Site.new(site_params)
-    client = Client.new(strong_params[:clients])
-    @worker = Worker.find(params[:worker_id])
-    position = Position.new(strong_params[:position])
-    position.company = Company.find(strong_params[:company][:company_id])
-    position.site = @site
-    @site.client = client
-    position.worker = @worker
-
-    if @site.save && position.save
-      redirect_to worker_path(@worker)
-    else
-      render '_new'
-    end
-  end
-
-  def destroy
-    @site.destroy
-    redirect_to sites_path
-  end
-
-  def edit
-  end
-
-  def index
-    @sites = Site.all
-  end
-
-  # def new
-  #   @site = Site.new
-  #   @worker = Worker.find(params[:worker_id])
-  #   @companies = Company.all
-  #   @site.company.build
-  #   # @site.position.build
-  #   # @site.client.build
+  # def destroy
+  #   @site.destroy
+  #   redirect_to sites_path
   # end
 
-  def show
-    set_site
-  end
+  # def edit
+  # end
 
-  def update
-    @site.update(site_params)
-    redirect_to site_path(@site)
-  end
+  # # def index
+  # #   @sites = Site.all
+  # # end
 
-  private
+  # def show
+  #   set_site
+  # end
 
-  def set_site
-    @site = Site.find(params[:id])
-  end
+  # def update
+  #   @site.update(site_params)
+  #   redirect_to site_path(@site)
+  # end
 
-  def strong_params
-    params.require(:site).permit(:name, :site_type, :start_date, :end_date, :amount, :money_unit, :birth_date, company: [ :company_id ], position: [ :position_name ], clients: [ :name ])
-  end
+  # private
 
-  def site_params
-    strong_params.except(:position, :clients, :company)
-  end
+  # def set_site
+  #   @site = Site.find(params[:id])
+  # end
+
+  # def strong_params
+  #   params.require(:site).permit(:name, :site_type, :start_date, :end_date, :amount, :money_unit, :birth_date, company: [ :company_id ], position: [ :position_name ], clients: [ :name ])
+  # end
+
+  # def site_params
+  #   strong_params.except(:position, :clients, :company)
+  # end
 end
