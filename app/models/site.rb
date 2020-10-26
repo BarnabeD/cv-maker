9f89c031376a544295c42221ada044c0a7392edf
Barnabe Dubus <barnabe.dubus@gmail.com>
2020-10-26 18:31:19 +0100

    add photo to models (site + user + worker)

--- app/models/site.rb
+++ app/models/site.rb
@@ -2,6 +2,7 @@ class Site < ApplicationRecord
   belongs_to :client
   has_many :positions
   has_many :workers, through: :positions, dependent: :destroy
+  has_one_attached :photo
   # accepts_nested_attributes_for :positions, :client
 
   validates :name, :site_type, :start_date, :end_date, :amount, :money_unit, presence: true
