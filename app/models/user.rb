9dd56b95d36b4a1dcdda6f4751fc1cff422aa319
Barnabe Dubus <barnabe.dubus@gmail.com>
2020-10-27 11:55:01 +0100

    add photo requirement to user + skip validates in seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :password_expirable,
          :secure_validatable, email_validation: false
 
+  validates :pohto, presence: true
   def admin?
     self.admin
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -91,10 +91,10 @@ ap position
 puts "Creating users"
 admin = User.new(email: "barnabe.dubus@gmail.com", password: "123456", password_confirmation: "123456", admin: true)
 admin.skip_confirmation!
-admin.save!
+admin.save!(validate: false)
 user = User.new(email: "nonadmin@gmail.com", password: "123456", password_confirmation: "123456", admin: false)
 user.skip_confirmation!
-user.save!
+user.save!(validate: false)
 puts "creating #{User.count} users : #{user.email}"
 ap user
 
