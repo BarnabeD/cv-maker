f4520af305186b2146459823791da04fc83f24b3
Barnabe Dubus <barnabe.dubus@gmail.com>
2020-10-27 17:34:07 +0100

    add upload photo to profil

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,11 +5,25 @@ class PagesController < ApplicationController
   end
 
   def show
-    @user = current_user
+    set_user
   end
 
   def update
-
+    set_user
+    if @user.update(strong_params)
+      redirect_to profil_path(@user)
+    else
+      redirect_to profil_path(@user)
+    end
   end
 
+  private
+
+  def strong_params
+    params.require(:user).permit(:photo)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
