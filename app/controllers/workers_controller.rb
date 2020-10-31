36b46940713fabaa24ba6c4a3140225b2c725e2f
Barnabe Dubus <barnabe.dubus@gmail.com>
2020-10-31 16:30:30 +0100

    refacto worker controller to put methods in model

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -32,37 +32,12 @@ class WorkersController < ApplicationController
     @graduate = Graduate.new
     @position.build_company
     @position.build_site
-    @age = calcul_duration_between_date_and_now(@worker.birth_date)[:years]
-    @hiring_time = calcul_duration_between_date_and_now(@worker.hire_date)[:years]
-
-    # experience since first graduate cans be with the last graduate with '.last' instead of '.first'
-    if @worker.graduates.count > 0
-      @last_graduate_date = @worker.graduates.order(:graduation_date).first.graduation_date
-    else
-      @last_graduate_date = nil
-    end
-    # @experience = calcul_duration_between_date_and_now(@last_graduate.graduation_date)[:years]
-    # @site = Site.new
-    # @sites = Site.all
-    @worker_positions = Position.where(worker: @worker)
-    @worker_graduates = Graduate.where(worker: @worker)
-    @worker_trainings = Training.where(worker: @worker)
   end
-
-  # def update
-  #   @worker.update(worker_params)
-  #   redirect_to worker_path(@worker)
-  # end
 
   private
 
   def set_worker
     @worker = Worker.find(params[:id])
-  end
-
-  def calcul_duration_between_date_and_now(date)
-    duration_in_second = (Date.today - date).to_i
-    ActiveSupport::Duration.build(duration_in_second * 24 * 3600).parts
   end
 
   def worker_params
