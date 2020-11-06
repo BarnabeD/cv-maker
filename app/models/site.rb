15b9b63fe3ee668077ab7e212dfa49daddef739a
Barnabe Dubus <barnabe.dubus@gmail.com>
2020-11-06 13:55:01 +0100

    add position show WIP

--- app/models/site.rb
+++ app/models/site.rb
@@ -32,4 +32,24 @@ class Site < ApplicationRecord
     self.confidence == 'false' ? true : false
   end
 
+  def human_duration
+    duration = calcul_duration_between_two_dates(self.start_date, self.end_date)
+    years = duration[:years]
+    months = duration[:months]
+    array = []
+    if years > 1
+      array << "#{years} ans"
+    elsif years == 1
+      array << "#{years} an"
+    end
+    array << "#{months} mois" if months >= 1
+    array.length > 1 ? array.join(', et ') : array.join('')
+  end
+
+  private
+
+  def calcul_duration_between_two_dates(start_date, end_date)
+    duration_in_second = (end_date - start_date).to_i
+    ActiveSupport::Duration.build(duration_in_second * 24 * 3600).parts
+  end
 end
