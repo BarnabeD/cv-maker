cb274e170311b81c30a8f7a13144f4569d3127d0
Barnabe Dubus <barnabe.dubus@gmail.com>
2020-11-10 15:01:19 +0100

    dependent destroy on worker photo

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -3,7 +3,7 @@ class Worker < ApplicationRecord
   has_many :graduates
   has_many :trainings
   has_many :sites, through: :positions
-  has_one_attached :photo
+  has_one_attached :photo, dependent: :destroy
 
   validates :first_name, :last_name, :birth_date, :hire_date, :matricule, presence: true
   validates :matricule, uniqueness: true
