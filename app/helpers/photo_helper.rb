da97b021f53c6906ec1d81daed75e5f8f8d6f7ab
Barnabe Dubus <barnabe.dubus@gmail.com>
2020-11-16 15:28:22 +0100

    add format condition in photo_elper

--- app/helpers/photo_helper.rb
+++ app/helpers/photo_helper.rb
@@ -12,6 +12,8 @@ module PhotoHelper
   def site_photo(site, opts = {})
     if site.photo.attached?
       cl_image_tag site.photo.key, opts
+    elsif formats == [:pdf]
+      wicked_pdf_image_tag image_url("fake/#{(1..26).to_a.sample}.jpg"), opts
     else
       # image_tag "#{['site_placeholder1.jpg', 'site_placeholder2.jpg'].sample}", opts
       # image_tag 'https://source.unsplash.com/collection/1066077', opts
