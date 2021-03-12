b2f9cf263f84f6e2849e93d1e7a77240026de9a3
Barnabe DUBUS <barnabe.dubus@gmail.com>
2021-03-12 17:54:05 +0100

    add test for missing position name

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -15,4 +15,11 @@ RSpec.describe Position, type: :model do
       expect(subject).to_not be_valid
     end
   end
+
+  context 'missing position_name' do
+    it 'is not valid' do
+      subject.position_name = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
